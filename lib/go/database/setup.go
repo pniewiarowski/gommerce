b485cf62b6eb303d7f5bc03fe7a156a45f9bfa93
PNiewiarowski <pawelniewiarowski2001@gmail.com>
2024-02-09 15:44:48 +0100

    [A] Change port type to string, database module.

--- lib/go/database/setup.go
+++ lib/go/database/setup.go
@@ -8,8 +8,8 @@ import (
 
 var DataBase *gorm.DB
 
-func Setup(host, user, password, databaseName string, port int) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d", host, user, password, databaseName, port)
+func Setup(host, user, password, databaseName, port string) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, databaseName, port)
 	db, err := gorm.Open(postgres.New(postgres.Config{DSN: dsn}), &gorm.Config{})
 
 	if err != nil {
