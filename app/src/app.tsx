9e2bdbe228a096e4c6013d83b525b1e3e275e770
PNiewiarowski <pawelniewiarowski2001@gmail.com>
2024-03-12 19:08:49 +0100

    [A] Provide theme into project and create home page component.

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {HomePage} from "./page";
+import {lightTheme} from "./theme";
 
 const App = (): React.JSX.Element => {
     return (
-        <div>
-            <h1>Gommerce</h1>
-        </div>
+        <React.Fragment>
+            <ThemeProvider theme={lightTheme}>
+                <CssBaseline enableColorScheme/>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element=<HomePage/>/>
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider>
+        </React.Fragment>
     );
 }
 
---
+++ app/src/page/home-page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Grid} from "@mui/material";
+
+const HomePage = (): React.JSX.Element => {
+    return (
+        <Grid>
+            Home Page
+        </Grid>
+    );
+}
+
+export default HomePage;
