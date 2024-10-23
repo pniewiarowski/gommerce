39699b6009dd3bcbb9074bdae6f80c2a351e7773
pniewiarowski <pawelniewiarowski2001@gmail.com>
2024-10-23 22:49:46 +0200

    [A] Fullfil CLI service scope contract.

--- cli/command/microservice/delete.py
+++ cli/command/microservice/delete.py
@@ -1,6 +1,25 @@
+import sys
+import os
+import shutil
+
 from command.base import BaseCommand
+from command.error import CommandIncorrectUsage, ServiceDoesNotExists
+
+from command.tool import get_path_to_gommerce
 
 
 class Delete(BaseCommand):
+    path: str
+
     def execute(self) -> str:
-        pass
+        if len(sys.argv) <= 2:
+            raise CommandIncorrectUsage
+
+        microservice_name: str = sys.argv[2]
+        self.path = f'{get_path_to_gommerce()}{os.sep}api{os.sep}{microservice_name}'
+
+        if not os.path.exists(self.path):
+            raise ServiceDoesNotExists
+
+        shutil.rmtree(self.path)
+        return f'Microservice {microservice_name} deleted from system.'
--- cli/command/microservice/run.py
+++ cli/command/microservice/run.py
@@ -1,6 +1,25 @@
-from command.base import BaseCommand
+import sys
+import os
+import subprocess
 
+from command.base import BaseCommand
+from command.error import CommandIncorrectUsage, ServiceDoesNotExists
+
+from command.tool import get_path_to_gommerce
 
 class Run(BaseCommand):
+    path: str
+
     def execute(self) -> str:
-        pass
+        if len(sys.argv) <= 2:
+            raise CommandIncorrectUsage
+
+        microservice_name: str = sys.argv[2]
+        self.path = f'{get_path_to_gommerce()}{os.sep}api{os.sep}{microservice_name}'
+        if not os.path.exists(self.path):
+            raise ServiceDoesNotExists
+
+        go_run = f'go run .'
+        subprocess.call(go_run, cwd=self.path, shell=True)
+
+        return f'Microservice {microservice_name} is running...'
