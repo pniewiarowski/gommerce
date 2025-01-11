8ccfbfd013d3dd1704e461531db2f79bc887372c
pniewiarowski <pawelniewiarowski2001@gmail.com>
2025-01-11 16:38:47 +0100

    [F] Add deploy on fly.

--- api/_shared/database/setup.go
+++ api/_shared/database/setup.go
@@ -1,17 +1,14 @@
 package database
 
 import (
-	"fmt"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DataBase *gorm.DB
 
-func Setup(host, user, password, databaseName string, port int) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d", host, user, password, databaseName, port)
-	db, err := gorm.Open(postgres.New(postgres.Config{DSN: dsn}), &gorm.Config{})
+func Setup(databaseURL string) {
+	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
