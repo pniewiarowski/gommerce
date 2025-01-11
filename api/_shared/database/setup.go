9c246d1e6fa4fb25b60d3dd4c95913f8bc55770a
pniewiarowski <pawelniewiarowski2001@gmail.com>
2025-01-11 12:29:23 +0100

    [F] Change database to postgresql.

--- api/_shared/database/setup.go
+++ api/_shared/database/setup.go
@@ -1,14 +1,17 @@
 package database
 
 import (
-	"gorm.io/driver/sqlite"
+	"fmt"
+
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DataBase *gorm.DB
 
 func Setup(host, user, password, databaseName string, port int) {
-	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d", host, user, password, databaseName, port)
+	db, err := gorm.Open(postgres.New(postgres.Config{DSN: dsn}), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
